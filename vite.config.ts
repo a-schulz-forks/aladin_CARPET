753b26350dbf3ccf9beeeba8626a3ae0d2c5d7d3
a-schulz <schulz.alexander2002@gmail.com>
2023-08-03 23:23:39 +0200

    added .env to specify API_URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,33 @@
-import { fileURLToPath, URL } from "node:url";
+import {fileURLToPath, URL} from "node:url";
 
-import { defineConfig } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ["GridLayout"].includes(tag)
+export default ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+        plugins: [
+            vue({
+                template: {
+                    compilerOptions: {
+                        isCustomElement: (tag) => ["GridLayout"].includes(tag)
+                    }
+                }
+            })
+        ],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url))
+            }
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: process.env.VITE_API_URL,
+                    changeOrigin: true
+                }
+            }
         }
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true
-      }
-    }
-  }
-});
+    });
+};
