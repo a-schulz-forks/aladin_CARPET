626b252b34177e4bd05d3e841c2aa7b069d52e0d
a-schulz <schulz.alexander2002@gmail.com>
2023-08-04 11:15:27 +0200

    removed logging

--- src/components/taskComponents/mixins/ActionButtons.vue
+++ src/components/taskComponents/mixins/ActionButtons.vue
@@ -37,8 +37,6 @@ export default {
       const { type, instruction, parameters } = props.actions[actionId];
 
       props.actionTypes[type](instruction, parameters);
-      console.log(props.actions[actionId])
-      console.log(props.actionTypes[typxe](instruction, parameters))
     };
 
     return { handleAction };
