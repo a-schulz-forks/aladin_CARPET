edf661a5941b39c223762aec03d2f3f2ab618aef
a-schulz <schulz.alexander2002@gmail.com>
2023-08-24 18:07:32 +0200

    sync definition and import, added first puml diagram

--- src/components/gamification/data/definition.ts
+++ src/components/gamification/data/definition.ts
@@ -1,52 +1,51 @@
 export interface ISkillsMapping {
-    [key: string]: {
-        skillId?: string,
-        distinction?: IDistinctionItem[]
-    }
+  [key: string]: {
+    skillId?: string,
+    distinction?: IDistinctionItem[]
+  };
 }
 
 export interface IDistinctionItem {
-    skillId: string;
+  skillId: string;
 
-    [key: string]: any;
+  [key: string]: any;
 }
 
 interface IGamificationDefinition {
-    [key: string]: {
-        projectId: string,
-        skillsMapping: ISkillsMapping
-    }
+  [key: string]: {
+    projectId: string,
+    skillsMapping: ISkillsMapping
+  };
 }
 
 export const definition: IGamificationDefinition = {
-    "Gozintograph": {
-        "projectId": "aladin",
-        "skillsMapping": {
-            "nodes__0__components__1": {
-                "skillId": "AufgabenparametrisierungundgenerierungSkill",
+  "Gozintograph": {
+    "projectId": "aladin",
+    "skillsMapping": {
+      "nodes__0__components__1": {
+        "skillId": "AufgabeParametrisierenSkill",
 
-            },
-            "nodes__6__components__0": {
-                "skillId": "EntschlsselnvonGozintographenSkill",
-            },
-            "nodes__7__components": {
-                // if key is present
-                "distinction": [
-                    {
-                        "skillId": "GesamtbedarfsmatrixSkill",
-                        "component": {"readOnly": false},
-                        "name": "Gesamtbedarfsmatrix"
-                    },
-                    {
-                        "skillId": "DirektbedarfsmatrixSkill",
-                        "component": {"readOnly": false},
-                        "name": "Direktbedarfsmatrix"
-                    }
-                ]
-            },
-            "nodes__7__components__3": {
-                "skillId": "SekundärbedarfsvektorSkill",
-            }
-        }
-    }
-}
+      },
+      "nodes__6__components__0": {
+        "skillId": "AdjazenzmatrixAblesenSkill",
+      },
+      "nodes__7__components": {
+        "distinction": [
+          {
+            "skillId": "GesamtbedarfsmatrixBestimmenSkill",
+            "component": { "readOnly": false },
+            "name": "Gesamtbedarfsmatrix",
+          },
+          {
+            "skillId": "DirektbedarfsmatrixMultiplizierenSkill",
+            "component": { "readOnly": false },
+            "name": "Direktbedarfsmatrix",
+          },
+        ],
+      },
+      "nodes__7__components__3": {
+        "skillId": "SekundärbedarfsvektorSkill",
+      },
+    },
+  },
+};
