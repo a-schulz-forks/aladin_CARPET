039092eeaadb74e7549662d2afda293224ac70e9
a-schulz <schulz.alexander2002@gmail.com>
2023-08-28 17:15:04 +0200

    configure one path of Informationsmanagement to test configuration

--- src/components/gamification/data/definition.ts
+++ src/components/gamification/data/definition.ts
@@ -1,54 +1,70 @@
 export interface ISkillsMapping {
-  [key: string]:
-    | { value: any, skillId?: string }
-    | { value: any, distinction?: IDistinctionItem[] };
+    [key: string]:
+        | { value: any, skillId?: string }
+        | { value: any, distinction?: IDistinctionItem[] };
 }
 
 export interface IDistinctionItem {
-  skillId: string;
+    skillId: string;
 
-  [key: string]: any;
+    [key: string]: any;
 }
 
 interface IGamificationDefinition {
-  [key: string]: {
-    projectId: string,
-    skillsMapping: ISkillsMapping
-  };
+    [key: string]: {
+        projectId: string,
+        skillsMapping: ISkillsMapping
+    };
 }
 
 export const definition: IGamificationDefinition = {
-  "Gozintograph": {
-    "projectId": "aladin",
-    "skillsMapping": {
-      "nodes__0__components__1__isValid": {
-        "value": true,
-        "skillId": "AufgabeParametrisierenSkill",
-
-      },
-      "nodes__6__components__0__isValid": {
-        "value": true,
-        "skillId": "AdjazenzmatrixAblesenSkill",
-      },
-      "nodes__7__components__(\\d+)__isValid": {
-        "value": true,
-        "distinction": [
-          {
-            "skillId": "GesamtbedarfsmatrixBestimmenSkill",
-            "component": { "readOnly": false },
-            "name": "Gesamtbedarfsmatrix",
-          },
-          {
-            "skillId": "DirektbedarfsmatrixMultiplizierenSkill",
-            "component": { "readOnly": false },
-            "name": "Direktbedarfsmatrix",
-          },
-        ],
-      },
-      "nodes__7__components__3__isValid": {
-        "value": true,
-        "skillId": "SekundaerbedarfsvektorBestimmenSkill",
-      },
+    "Gozintograph": {
+        "projectId": "aladin",
+        "skillsMapping": {
+            "nodes__0__components__1__isValid": {
+                "value": true,
+                "skillId": "AufgabeParametrisierenSkill",
+            },
+            "nodes__6__components__0__isValid": {
+                "value": true,
+                "skillId": "AdjazenzmatrixAblesenSkill",
+            },
+            "nodes__7__components__(\\d+)__isValid": {
+                "value": true,
+                "distinction": [
+                    {
+                        "skillId": "GesamtbedarfsmatrixBestimmenSkill",
+                        "component": {"readOnly": false},
+                        "name": "Gesamtbedarfsmatrix",
+                    },
+                    {
+                        "skillId": "DirektbedarfsmatrixMultiplizierenSkill",
+                        "component": {"readOnly": false},
+                        "name": "Direktbedarfsmatrix",
+                    },
+                ],
+            },
+            "nodes__7__components__3__isValid": {
+                "value": true,
+                "skillId": "SekundaerbedarfsvektorBestimmenSkill",
+            },
+        },
     },
-  },
+    "Scheduling": {
+        "projectId": "aladin",
+        "skillsMapping": {
+            "nodes__0__components__0__isValid": {
+                "value": true,
+                "skillId": "AufgabeParametrisierenSkill",
+            },
+            "nodes__2__components__0__isValid": {
+                "value": true,
+                "skillId": "MetraPotentialMethodeSkill",
+            },
+            "nodes__3__components__0__isValid": {
+                "value": true,
+                "skillId": "ZeitanalyseDurchfuehrenSkill",
+            },
+        }
+    },
 };
