c29385edce7222cbe399880332775a34bf025314
a-schulz <schulz.alexander2002@gmail.com>
2023-08-28 16:29:22 +0200

    reset partial update with master

--- src/components/taskComponents/DOTGraph.vue
+++ src/components/taskComponents/DOTGraph.vue
@@ -2,37 +2,43 @@
   <div class="dotGraph" :id="`graph_${componentID}`"></div>
 </template>
 
-<script lang="ts" setup>
-import {onMounted, computed, watch} from "vue";
-import {graphviz} from "d3-graphviz";
-import type {ComponentProps} from "@/interfaces/ComponentInterface";
+<script lang="ts">
+import { onMounted, computed, watch } from "vue";
+import { graphviz } from "d3-graphviz";
 
-const props = defineProps<ComponentProps>();
+export default {
+  props: {
+    componentID: Number,
+    storeObject: Object
+  },
+  setup(props) {
+    const { store, getProperty } = props.storeObject;
+    const currentNode = computed(() => store.state.currentNode);
+    const path = `nodes__${currentNode.value}__components__${props.componentID}`;
 
-const {store, getProperty} = props.storeObject;
-const currentNode = computed(() => store.state.currentNode);
-const path = `nodes__${currentNode.value}__components__${props.componentID}`;
+    const dependencies = getProperty(`${path}__dependencies`);
+    const dotDescription = computed(() => {
+      const dotDescription = getProperty(dependencies.DOTGraph.dotDescription);
+      if (!dotDescription) return "";
+      return dotDescription;
+    });
 
-const dependencies = getProperty(`${path}__dependencies`);
-const dotDescription = computed(() => {
-  const dotDescription = getProperty(dependencies.DOTGraph.dotDescription);
-  if (!dotDescription) return "";
-  return dotDescription;
-});
-
-const renderGraph = (description: string) => {
-  graphviz(`#graph_${props.componentID}`, {
-    fit: true,
-    zoom: false,
-    useWorker: false
-  }).renderDot(description);
+    const renderGraph = (description) => {
+      graphviz(`#graph_${props.componentID}`, {
+        fit: true,
+        zoom: false,
+        useWorker: false
+      }).renderDot(description);
+    };
+    watch(dotDescription, () => {
+      renderGraph(dotDescription.value);
+    });
+    onMounted(() => {
+      renderGraph(dotDescription.value);
+    });
+    return {};
+  }
 };
-watch(dotDescription, () => {
-  renderGraph(dotDescription.value);
-});
-onMounted(() => {
-  renderGraph(dotDescription.value);
-});
 </script>
 
 <style>
--- src/components/taskComponents/EditableGraph.vue
+++ src/components/taskComponents/EditableGraph.vue
@@ -1,130 +1,94 @@
 <template>
   <ContextMenu :componentId="id" :methods="selectedMethods" :storeObject="storeObject">
     <div class="editableGraph">
-      <DOTGraph :componentID="componentID" :storeObject="storeObject" />
+      <DOTGraph :componentID="componentID" :storeObject="storeObject"/>
     </div>
   </ContextMenu>
 </template>
 
-<script lang="ts" setup>
-import { onMounted, onUnmounted, computed } from "vue";
+<script lang="ts">
+import {onMounted, onUnmounted, computed} from "vue";
 import DOTGraph from "@/components/taskComponents/DOTGraph.vue";
 import ContextMenu from "@/components/taskComponents/mixins/ContextMenu.vue";
-import { pollGraphRender } from "@/helpers/HelperFunctions";
-<<<<<<< HEAD
-import type { ComponentProps } from "@/interfaces/ComponentInterface";
-=======
+import {pollGraphRender} from "@/helpers/HelperFunctions";
 import {getSelectedMethods} from "@/helpers/getSelectedMethods";
->>>>>>> 26e34591a0f242b9d5b7b3d09c42bd58bc5c72f8
 
-const props = defineProps<ComponentProps>();
+export default {
+  components: {DOTGraph, ContextMenu},
+  props: {
+    componentID: Number,
+    storeObject: Object,
+  },
+  setup(props) {
+    const {getProperty, setProperty} = props.storeObject;
+    const currentNode = getProperty("currentNode");
+    const path = `nodes__${currentNode}__components__${props.componentID}`;
+    const dependencies = getProperty(`${path}__dependencies`);
 
-const { getProperty, setProperty } = props.storeObject;
-const currentNode = getProperty("currentNode");
-const path = `nodes__${currentNode}__components__${props.componentID}`;
-const dependencies = getProperty(`${path}__dependencies`);
+    const userValues = computed(() => getProperty(`${path}__component__userValues`));
+    if (!userValues.value)
+      setProperty({path: `${path}__component__userValues`, value: [...getProperty(dependencies.EditableGraph.validation)]});
 
-const userValues = computed(() => getProperty(`${path}__component__userValues`));
-if (!userValues.value)
-  setProperty({ path: `${path}__component__userValues`, value: [...getProperty(dependencies.EditableGraph.validation)] });
+    const editSVGText = (event) => {
+      const target = event.target;
+      const propertyNode = target.parentElement.parentElement;
+      const node = propertyNode.parentElement.parentElement;
+      const nodeProperty = propertyNode.getAttribute("id").split("_").pop().trim();
+      const nodeId = node.getAttribute("id").split("_").pop().trim();
 
-const editSVGText = (event: Event) => {
-  const target = <HTMLElement>event.target;
-  const propertyNode = <HTMLElement>target?.parentElement?.parentElement;
-  const node = <HTMLElement>propertyNode.parentElement?.parentElement;
-  const nodeProperty = <string>propertyNode.getAttribute("id")?.split("_")?.pop()?.trim();
-  const nodeId = node?.getAttribute("id")?.split("_")?.pop()?.trim();
+      const input = document.createElement("input");
+      input.style.textAlign = "right";
+      input.style.width = "20px";
+      input.value = target.textContent;
+      input.onkeyup = (event) => {
+        const input = (event.target as unknown) as HTMLInputElement;
+        if (["Enter", "Escape"].includes(event.key)) {
+          input.blur();
+          return;
+        }
+        // TODO eventually shift logic into deep watcher, to make replays possible
+        target.textContent = input.value;
+        setProperty({path: `${path}__component__userValues__${nodeId}__${nodeProperty}`, value: input.value});
+      };
+      input.onblur = () => {
+        foreignObject.remove();
+        validate();
+      };
 
-  const input = document.createElement("input");
-  input.style.textAlign = "right";
-  input.style.width = "20px";
-  input.value = <string>target.textContent;
-  input.onkeyup = (event) => {
-    const input = event.target as unknown as HTMLInputElement;
-    if (["Enter", "Escape"].includes(event.key)) {
-      input.blur();
-      return;
-    }
-    // TODO eventually shift logic into deep watcher, to make replays possible
-    target.textContent = input.value;
-    setProperty({ path: `${path}__component__userValues__${nodeId}__${nodeProperty}`, value: input.value });
-  };
-  input.onblur = () => {
-    foreignObject.remove();
-    validate();
-  };
+      const foreignObject = document.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
+      foreignObject.setAttribute("width", "100%");
+      foreignObject.setAttribute("height", "100%");
+      foreignObject.setAttribute("y", `${target.getAttribute("y") - 15}`);
+      foreignObject.setAttribute("x", target.getAttribute("x"));
+      foreignObject.append(input);
 
-  const foreignObject = document.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
-  foreignObject.setAttribute("width", "100%");
-  foreignObject.setAttribute("height", "100%");
-  foreignObject.setAttribute("y", `${parseInt(<string>target?.getAttribute("y")) - 15}`);
-  foreignObject.setAttribute("x", <string>target?.getAttribute("x"));
-  foreignObject.append(input);
+      const svg = target.parentNode;
+      svg.append(foreignObject);
 
-  const svg = target.parentNode;
-  svg?.append(foreignObject);
+      input.select();
+    };
 
-  input.select();
-};
+    const editableFields = getProperty(`${path}__component__editableFields`);
+    const selectors = editableFields.map((field) => `g.node g[id="a_${field}"] text`);
+    const assignEventHandlers = () => {
+      Array.from(document.querySelectorAll(selectors)).forEach((node) => {
+        node.setAttribute("pointer-events", "visible");
+        node.addEventListener("click", editSVGText);
+      });
+    };
 
-const editableFields: Array<string> = getProperty(`${path}__component__editableFields`);
-const selectors = editableFields.map((field) => `g.node g[id="a_${field}"] text`);
-const assignEventHandlers = () => {
-  Array.from(document.querySelectorAll(selectors)).forEach((node) => {
-    node.setAttribute("pointer-events", "visible");
-    node.addEventListener("click", editSVGText);
-  });
-};
-
-onMounted(() => {
-  pollGraphRender(".editableGraph .node", assignEventHandlers);
-  pollGraphRender(".editableGraph .node", validate);
-});
-onUnmounted(() => {
-  Array.from(document.querySelectorAll(selectors)).forEach((node) => node.removeEventListener("click", editSVGText));
-});
-
-const validate = () => {
-  const nodes = getProperty(dependencies.EditableGraph.validation);
-  const isValid = nodes.every((node) => {
-    const { id } = node;
-    // TODO make editableFields Array of keys again and create replica of nodes for userValue to record changes in VUEX state and add node id to handler
-    // create deep watcher to extract the changed key<->value in new/old Value of watcher
-    // move assignment logic of textfield from the editSVGText-handler to watcher
-    return editableFields.every((field) => {
-      const correctValue = node[field];
-      const userValue = document.querySelector(`g.node[id="${id}"] g[id="a_${field}"] text`)?.textContent?.trim();
-      return userValue == correctValue;
+    onMounted(() => {
+      pollGraphRender(".editableGraph .node", assignEventHandlers);
+      pollGraphRender(".editableGraph .node", validate);
     });
-  });
-
-  setProperty({
-    path: `${path}__isValid`,
-    value: isValid
-  });
-};
-
-const methods = {
-  showSolution: () => {
-    // manually remove old svg, since foreignObjects might cause issues with the rerender
-    Array.from(document.querySelectorAll(selectors)).forEach((node) => node.removeEventListener("click", editSVGText));
-    const svg = document.querySelector(".dotGraph svg");
-    const dotGraph = getProperty(dependencies.DOTGraph.dotDescription);
-    const solution = getProperty(dependencies.EditableGraph.solution);
-    if (svg && dotGraph != solution) document.querySelector(".dotGraph")?.removeChild(svg);
-
-    setProperty({ path: dependencies.DOTGraph.dotDescription, value: solution });
-
-    pollGraphRender(".editableGraph .node", assignEventHandlers);
-    setProperty({
-      path: `${path}__isValid`,
-      value: true
+    onUnmounted(() => {
+      Array.from(document.querySelectorAll(selectors)).forEach((node) => node.removeEventListener("click", editSVGText));
     });
 
     const validate = () => {
       const nodes = getProperty(dependencies.EditableGraph.validation);
       const isValid = nodes.every((node) => {
-        const { id } = node;
+        const {id} = node;
         // TODO make editableFields Array of keys again and create replica of nodes for userValue to record changes in VUEX state and add node id to handler
         // create deep watcher to extract the changed key<->value in new/old Value of watcher
         // move assignment logic of textfield from the editSVGText-handler to watcher
@@ -150,7 +114,7 @@ const methods = {
         const solution = getProperty(dependencies.EditableGraph.solution);
         if (svg && dotGraph != solution) document.querySelector(".dotGraph").removeChild(svg);
 
-        setProperty({ path: dependencies.DOTGraph.dotDescription, value: solution });
+        setProperty({path: dependencies.DOTGraph.dotDescription, value: solution});
 
         pollGraphRender(".editableGraph .node", assignEventHandlers);
         setProperty({
